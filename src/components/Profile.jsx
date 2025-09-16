1f0263f16472f3636b17bbb810d60d197dc29650
Phsithzz <phoosith2548@gmail.com>
2025-09-16 12:55:25 +0700

    Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,11 @@
 // icon
 import { FaUserCircle } from "react-icons/fa";
 
-const Profile = ({ onUserClick }) => {
+const Profile = ({ onCancel }) => {
   return (
     <div className="w-fit bg-white rounded-xl px-4 py-2 shadow-md transition-all ease-in duration-200 hover:bg-black hover:text-white hover:shadow-lg">
       <button
-        onClick={onUserClick}
+        onClick={onCancel}
         type="button"
         className="flex items-center gap-2 cursor-pointer"
       >
